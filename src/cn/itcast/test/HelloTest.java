acbe3cedc21a0fb8bef83422d734f90d494590fd
xiaobu <dp825496966@qq.com>
2019-02-20 19:50:17 +0800

    把本地工程添加都git

---
+++ src/cn/itcast/test/HelloTest.java
@@ -0,0 +1,18 @@
+package cn.itcast.test;
+
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+        System.out.println("project........");
+    }
+}
